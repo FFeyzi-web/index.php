3a33202472f1e6bf06ea45ac41a123f478b207cd
FFeyzi-web <feyzullayevferqn@gmail.com>
2026-07-16 18:04:49 +0400

    protected

--- OOP.php
+++ OOP.php
@@ -13,24 +13,24 @@
 
 //OOP - Object Oriented Proqraming 
 
-class Riyaziyyat {
+// class Riyaziyyat {
 
-    public $eded1 = 4;
-    public $eded2 = 2;
+//     public $eded1 = 4;
+//     public $eded2 = 2;
 
-    public function topla() {
-        return $this -> eded1 + $this->eded2;
+//     public function topla() {
+//         return $this->eded1 + $this->eded2;
        
-       }
-       public function topla() {
-        return $this -> eded1 + $this->eded2;
+//        }
+//     public function vurma() {
+//         return $this->eded1 * $this->eded2;
+//        }
+//     //private
+//     //protected
 
-    //private
-    //protected
-
-}
-$riyaziyyatFenni = new Riyaziyyat();
-echo $riyaziyyatFenni -> topla();
+// }
+// $riyaziyyatFenni = new Riyaziyyat();
+// echo $riyaziyyatFenni->topla();
 
 
 
@@ -40,16 +40,73 @@ echo $riyaziyyatFenni -> topla();
 //klass yaratmaq ve klasin obyektini yaratmaq ayri-ayri islerdir
 
 // $car = new Car();
-// $car ->start();
-// $car ->brand = 'bmw <br>';
-// // echo $car ->brand;
+// $car->start();
+// $car->brand = 'bmw <br>';
+// // echo $car->brand;
 
 
 
 // $car2 = new Car();
 // $car2->brand = 'opel';
 // $car2->start();
-// echo $car2 -> brand ;
+// echo $car2->brand ;
+
+
+
+
+
+//public
+//protected
+//private
+
+// class Test {
+
+//     public $name = "Artyom";
+    
+//     public function sayHello() {
+
+//         return $this->name . "Salam";
+
+//     }
+
+
+// }
+
+
+// $test = new Test();
+
+// echo $test->name . "<br>";
+// echo $test->sayHello(). "<br>";
+
+
+
+
+class Test {
+
+    protected $name = "Artyom";
+    
+    protected function sayHello() {
+        return $this->name . "Salam";
+    }
+
+
+}
+
+class ChildTest extends Test {
+
+    public function sayHelloFromChild() {
+        return $this->sayHello();
+    }
+}
+
+
+
+
+
+$test = new Test();
+$childTest = new ChildTest();
+echo $childTest->sayHelloFromChild() . "<br>";
+
 
 
 
